2719cd3881a52a52376f4d609faec5fc73691f77
kriscfoster <kfoster@salesforce.com>
2021-01-04 10:59:21 +0000

    formatting

--- src/main/kotlin/com/kriscfoster/pictionary/word/Word.kt
+++ src/main/kotlin/com/kriscfoster/pictionary/word/Word.kt
@@ -6,9 +6,7 @@ import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
-data class Word(
-        var name: String? = null
-) {
+data class Word(var name: String? = null) {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
 }
