053508ef57ebbdfdced403796f98503e93e99376
kriscfoster <krisfoster@live.ie>
2021-01-18 07:38:16 +0000

    JSON & Thymeleaf

--- src/main/kotlin/com/kriscfoster/pictionary/word/WordController.kt
+++ src/main/kotlin/com/kriscfoster/pictionary/word/WordController.kt
@@ -1,23 +1,31 @@
 package com.kriscfoster.pictionary.word
 
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.http.MediaType.TEXT_HTML_VALUE
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
-@RequestMapping
 class WordController(val wordService: WordService) {
 
-    @GetMapping
+    @GetMapping("/api/words", produces= [APPLICATION_JSON_VALUE])
+    @ResponseBody
     fun getWords(
+            @RequestParam(value = "count", defaultValue = "1") count: Int,
+    ) : List<Word> {
+        return wordService.getRandomWords(count)
+    }
+
+    @GetMapping(produces = [TEXT_HTML_VALUE])
+    fun getWordsView(
             @RequestParam(value = "count", defaultValue = "1") count: Int,
             model: Model
     ) : String {
-        val words = wordService.getRandomWords(count)
         model.addAttribute("count", listOf(1, 3, 5))
-        model.addAttribute("words", words)
+        model.addAttribute("words", getWords(count))
         return "word"
     }
 }
